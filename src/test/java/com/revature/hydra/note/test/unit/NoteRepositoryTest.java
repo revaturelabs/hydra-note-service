e9b3bbca5f408d5b5b90a7ae65a1aea5cd499b71
Matthew Little <mlittl9199@gmail.com>
2018-02-28 15:02:34 -0500

    trying to run tests

--- src/test/java/com/revature/hydra/note/test/unit/NoteRepositoryTest.java
+++ src/test/java/com/revature/hydra/note/test/unit/NoteRepositoryTest.java
@@ -9,15 +9,16 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.revature.beans.NoteType;
 import com.revature.beans.SimpleNote;
 import com.revature.hydra.note.data.NoteRepository;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 public class NoteRepositoryTest {
 	private static final int TEST_TRAINEE_ID = 5529;
 	private static final int TEST_BATCH_ID = 2100;
